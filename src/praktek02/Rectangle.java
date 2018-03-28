15347951952c1d3e1906e87b7fc188e254f3168f
firdiseptiady <firdis998@gmail.com>
2018-03-28 23:18:07 +0800

    membuat atribut behavior

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -1,5 +1,13 @@
 package praktek02;
 
 public class Rectangle {
+    Integer Harga;
+    Integer Jumlah_Barang;
     
+    void cetakInfo() {
+        System.out.println("=====================");
+        System.out.println("Harga :"+Harga);
+        System.out.println("Jumlah_Barang :"+Jumlah_Barang);
+        System.out.println("=====================");
+    }
 }
